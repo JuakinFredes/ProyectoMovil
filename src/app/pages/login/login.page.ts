5556a359c925739eac1dbf03d38e15e796638ab5
AnSan <abelen.sanhuezap@gmail.com>
2024-09-24 22:02:43 -0300

    pruebaa

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -9,10 +9,12 @@ import { ToastController } from '@ionic/angular';
 })
 
 export class LoginPage implements OnInit {
+  //declarar un modelo para obtener los input del login
   login:any={
    usuario:"",
    password:"" 
  }
+ //defino una variable para indicar el campo vacío
  field:string="";
    constructor(public router:Router, public toastController:ToastController) { }
  
@@ -27,6 +29,7 @@ export class LoginPage implements OnInit {
   ingresar(){
     if(this.validateModel(this.login)){
       this.presentToast("top","Bienvenid@");
+      //agrego creación de NavigationExtras para pasar parámetros a ota page
       let navigationExtras : NavigationExtras ={
         state: {login: this.login}
       };
@@ -38,9 +41,16 @@ export class LoginPage implements OnInit {
 
 
 
+  /**
+     * validateModel sirve para validar que se ingrese algo en los
+     * campos del html mediante su modelo
+     */
   validateModel(model:any){
+    //Recorro el modelo 'login' revisando las entradas del Object
     for(var [key,value] of Object.entries(model)){
+      //si un valor es "" retorno falso e indico el nombre del campo que falta
       if(value == ""){
+        //rescato el nombre del campo vacío
         this.field = key;
         return false;
       }
