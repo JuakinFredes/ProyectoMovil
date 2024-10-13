d5b3987953ab43d64be7894bad6bf8f6949bb90e
juakin <juakinb3rnardo@gmail.com>
2024-10-13 18:49:41 -0300

    instalacion de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule,AngularFireAuthModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAt9cFPLlWwKl2chdpXeboMNOOM7qoaOEQ",
+    authDomain: "proyecto-movil-5ef0b.firebaseapp.com",
+    projectId: "proyecto-movil-5ef0b",
+    storageBucket: "proyecto-movil-5ef0b.appspot.com",
+    messagingSenderId: "675087875380",
+    appId: "1:675087875380:web:469c5ceb31be3495127961",
+    measurementId: "G-MBWJF88SWF"
+  }
+  
 };
 
 /*
