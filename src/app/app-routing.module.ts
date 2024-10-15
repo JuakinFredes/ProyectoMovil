aa62ced5e731699c0b22aad87d82a9f78d2449c2
juakin <juakinb3rnardo@gmail.com>
2024-10-15 15:42:36 -0300

    solucion del authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
-
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
