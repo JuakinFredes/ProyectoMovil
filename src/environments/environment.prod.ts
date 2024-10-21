15537a03e049c97be601651b66c009358b9f58a8
AnSan <abelen.sanhuezap@gmail.com>
2024-10-20 21:53:52 -0300

    deshabilitacion errores tipo estricto en tsconfig.json y corrección error Property 'firebaseConfig' does not exist on type '{ production: boolean; }

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAt9cFPLlWwKl2chdpXeboMNOOM7qoaOEQ",
+    authDomain: "proyecto-movil-5ef0b.firebaseapp.com",
+    projectId: "proyecto-movil-5ef0b",
+    storageBucket: "proyecto-movil-5ef0b.appspot.com",
+    messagingSenderId: "675087875380",
+    appId: "1:675087875380:web:469c5ceb31be3495127961",
+    measurementId: "G-MBWJF88SWF"
+  }
+  
 };
